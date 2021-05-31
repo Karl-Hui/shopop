d1ce9d7cefa16fcedf9ef6f36a43ae1aa9d741ba
Karl-Hui <KarlHui0311@gmail.com>
2021-06-01 01:13:44 +0800

    can grab user signup data

---
+++ passport/customerSerilize.js
@@ -0,0 +1,26 @@
+const database = require("../knexfile").development;
+const knex = require("knex")(database);
+
+function serializeUser(user, done) {
+  if (Array.isArray(user)) {
+    done(null, user[0].id);
+  } else {
+    done(null, user.id);
+  }
+}
+
+function deserializeUser(id, done) {
+  // console.log("+++++++++++", user);
+  // console.log("Deserialize user!", id);
+  knex("customer")
+    .select("*")
+    .where({ id: id })
+    .then((user) => {
+      console.log("deserializeUser: ", user[0]);
+      done(null, user[0]);
+    });
+}
+module.exports = {
+  serializeUser: serializeUser,
+  deserializeUser: deserializeUser,
+};
