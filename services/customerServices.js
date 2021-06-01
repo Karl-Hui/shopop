227848920d4f4845b031701d0d2458c474b57925
Karl-Hui <KarlHui0311@gmail.com>
2021-06-01 21:49:06 +0800

    k

--- services/customerServices.js
+++ services/customerServices.js
@@ -1,30 +1,31 @@
-require("dotenv").config();
+require("dotenv").config({ path: "../.env" });
+
 const database = require("../knexfile").development;
 const knex = require("knex")(database);
 
-const Table_Name = "customer"
+const Table_Name = "customer";
 
 class CustomerServices {
   constructor(knex) {
     this.knex = knex;
   }
 
-getCustomerName(customerId) {
-return this.knex("customer")
-.select()
-.where({id:customerId})
-.then((data) =>{
-  console.log("this is customer data:",data[0].username)
-  return data[0].username
-})
-.catch((error) => {
-  console.log("error", error);
-})
-}
-
+  getCustomerName(customerId) {
+    return this.knex("customer")
+      .select()
+      .where({ id: customerId })
+      .then((data) => {
+        console.log("this is customer data:", data[0].username);
+        return data[0].username;
+      })
+      .catch((error) => {
+        console.log("error", error);
+      });
+  }
 }
 //test
-// let service = new CustomerServices(knex)
-// service.getCustomerName(1)
+let service = new CustomerServices(knex);
+service.getCustomerName(1);
+// console.log("hi");
 
 module.exports = CustomerServices;
