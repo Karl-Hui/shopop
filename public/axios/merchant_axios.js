4c9981442bf17707052e7729c77ec6d762546069
Karl-Hui <KarlHui0311@gmail.com>
2021-06-14 20:43:13 +0800

    fix bugs

--- public/axios/merchant_axios.js
+++ public/axios/merchant_axios.js
@@ -1,42 +1,36 @@
-
-
 //button to update merchant username
-  let editMerchantbtn = document.querySelector("#updateMerchantName")
+let editMerchantbtn = document.querySelector("#updateMerchantName");
 
-editMerchantbtn.addEventListener('click',() => {
-    console.log("hello you clicked me")
-    let edit = {
-        merchantName:document.querySelector("#merchantName").value
-    }
-    // console.log("this is edit", edit);
-    axios.put('/shop/merchant-settings',edit)
-    .then((data)=> {
-        document.querySelector("#merchant-words").innerHTML=data.data
-        // window.location.reload();
-        // console.log("edit from axios username")
+editMerchantbtn.addEventListener("click", () => {
+  console.log("hello you clicked me");
+  let edit = {
+    merchantName: document.querySelector("#merchantName").value,
+  };
+  axios
+    .put("/shop/merchant-settings", edit)
+    .then((data) => {
+      document.querySelector("#merchant-words").innerHTML = data.data;
     })
-    .catch((error)=> {
-        console.log("error", error);
-    })
+    .catch((error) => {
+      console.log("error", error);
+    });
 });
 
+let editMerchantDescriptionbtn = document.querySelector("#Description");
 
-let editMerchantDescriptionbtn = document.querySelector("#Description")
-
-editMerchantDescriptionbtn.addEventListener('click',() => {
-    console.log("hello you clicked me")
-    let edit = {
-        shopDescription:document.querySelector("#updateShopDescription").value
-    }
-    console.log("this is the  edit data", edit);
-    axios.put('/shop/merchant-setting',edit)
-    .then((DATA)=> {
-        console.log(DATA)
-        document.querySelector("#shopDescription-words").innerHTML=DATA.data
-        // window.location.reload();
-        // console.log("edit from axios shopUpdate description")
+editMerchantDescriptionbtn.addEventListener("click", () => {
+  console.log("hello you clicked me");
+  let edit = {
+    shopDescription: document.querySelector("#updateShopDescription").value,
+  };
+  console.log("this is the  edit data", edit);
+  axios
+    .put("/shop/merchant-setting", edit)
+    .then((DATA) => {
+      console.log(DATA);
+      document.querySelector("#shopDescription-words").innerHTML = DATA.data;
     })
-    .catch((error)=> {
-        console.log("error", error);
-    })
+    .catch((error) => {
+      console.log("error", error);
+    });
 });
--- public/cart_js/stripe-cart.js
+++ public/cart_js/stripe-cart.js
@@ -1,141 +1,182 @@
 if (document.readyState == "loading") {
-    document.addEventListener("DOMContentLoaded", ready);
+  document.addEventListener("DOMContentLoaded", ready);
 } else {
-    ready();
+  ready();
 }
 
 function ready() {
-    // A reference to Stripe.js initialized with your real test publishable API key.
-    let stripe = Stripe("pk_test_51IxRKcFJ5Zy0PmAX77X8VSEoJniVJO2KdSzuJKBvzJ0qutaYD7TxzMvgzWRdOFfxyW2WcdZBuZDhBalUZsVwGhYO00sDTgEPNg");
+  // A reference to Stripe.js initialized with your real test publishable API key.
+  let stripe = Stripe(
+    "pk_test_51IxRKcFJ5Zy0PmAX77X8VSEoJniVJO2KdSzuJKBvzJ0qutaYD7TxzMvgzWRdOFfxyW2WcdZBuZDhBalUZsVwGhYO00sDTgEPNg"
+  );
 
-    // // The items the customer wants to buy
-    // let purchase = {
-    //     items: [{
-    //         id: "xl-tshirt"
-    //     }]
-    // };
+  // // The items the customer wants to buy
+  // let purchase = {
+  //     items: [{
+  //         id: "xl-tshirt"
+  //     }]
+  // };
 
-    // Disable the button until we have Stripe set up on the page
-    // document.querySelector(".checkoutBtn").disabled = true;
+  // Disable the button until we have Stripe set up on the page
+  // document.querySelector(".checkoutBtn").disabled = true;
 
-    let checkoutBtn = document.querySelector(".checkout-Btn")
-    checkoutBtn.addEventListener("click", () => {
-        fetch("/merchant/stripe/payment", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-                // body: JSON.stringify(purchase)
-            })
-            .then(function (result) {
-                return result.json();
-            })
-            .then(function (data) {
-                let elements = stripe.elements();
-
-                let style = {
-                    base: {
-                        color: "#32325d",
-                        fontFamily: 'Arial, sans-serif',
-                        fontSmoothing: "antialiased",
-                        fontSize: "16px",
-                        "::placeholder": {
-                            color: "#32325d"
-                        }
-                    },
-                    invalid: {
-                        fontFamily: 'Arial, sans-serif',
-                        color: "#fa755a",
-                        iconColor: "#fa755a"
-                    }
-                };
-
-                let card = elements.create("card", {
-                    style: style
-                });
-                // Stripe injects an iframe into the DOM
-                card.mount("#card-element");
-
-                card.on("change", function (event) {
-                    // Disable the Pay button if there are no card details in the Element
-                    document.querySelector(".checkoutBtn").disabled = event.empty;
-                    document.querySelector("#card-error").textContent = event.error ? event.error.message : "";
-                });
-
-                let form = document.getElementById("payment-form");
-                form.addEventListener("submit", function (event) {
-                    event.preventDefault();
-                    // Complete payment when the submit button is clicked
-                    payWithCard(stripe, card, data.clientSecret);
-                });
-            });
-
+  let checkoutBtn = document.querySelector(".checkout-Btn");
+  checkoutBtn.addEventListener("click", () => {
+    fetch("/merchant/stripe/payment", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      // body: JSON.stringify(purchase)
     })
+      .then(function (result) {
+        return result.json();
+      })
+      .then(function (data) {
+        let elements = stripe.elements();
 
-    // Calls stripe.confirmCardPayment
-    // If the card requires authentication Stripe shows a pop-up modal to
-    // prompt the user to enter authentication details without leaving your page.
-    let payWithCard = function (stripe, card, clientSecret) {
-        loading(true);
-        stripe
-            .confirmCardPayment(clientSecret, {
-                payment_method: {
-                    card: card
-                }
-            })
-            .then(function (result) {
-                if (result.error) {
-                    // Show error to your customer
-                    showError(result.error.message);
-                } else {
-                    // The payment succeeded!
-                    orderComplete(result.paymentIntent.id);
-                    fetch("/merchant/stripe/checkInfo", {
-                        method: 'post'
-                    })
-                    // fetch.post("/merchant/stripe/checkInfo")
-                }
-            });
-    };
+        let style = {
+          base: {
+            color: "#32325d",
+            fontFamily: "Arial, sans-serif",
+            fontSmoothing: "antialiased",
+            fontSize: "16px",
+            "::placeholder": {
+              color: "#32325d",
+            },
+          },
+          invalid: {
+            fontFamily: "Arial, sans-serif",
+            color: "#fa755a",
+            iconColor: "#fa755a",
+          },
+        };
 
-    /* ------- UI helpers ------- */
+        let card = elements.create("card", {
+          style: style,
+        });
+        // Stripe injects an iframe into the DOM
+        card.mount("#card-element");
 
-    // Shows a success message when the payment is complete
-    let orderComplete = function (paymentIntentId) {
-        loading(false);
-        document
-            .querySelector(".result-message a")
-            .setAttribute(
-                "href",
-                "https://dashboard.stripe.com/test/payments/" + paymentIntentId
-            );
-        document.querySelector(".result-message").classList.remove("hidden");
-        document.querySelector(".checkoutBtn").disabled = true;
+        card.on("change", function (event) {
+          // Disable the Pay button if there are no card details in the Element
+          document.querySelector(".checkoutBtn").disabled = event.empty;
+          document.querySelector("#card-error").textContent = event.error
+            ? event.error.message
+            : "";
+        });
 
-    };
+        let form = document.getElementById("payment-form");
+        form.addEventListener("submit", function (event) {
+          event.preventDefault();
+          // Complete payment when the submit button is clicked
+          payWithCard(stripe, card, data.clientSecret);
+        });
+      });
+  });
 
-    // Show the customer the error from Stripe if their card fails to charge
-    let showError = function (errorMsgText) {
-        loading(false);
-        let errorMsg = document.querySelector("#card-error");
-        errorMsg.textContent = errorMsgText;
-        setTimeout(function () {
-            errorMsg.textContent = "";
-        }, 4000);
-    };
-
-    // Show a spinner on payment submission
-    let loading = function (isLoading) {
-        if (isLoading) {
-            // Disable the button and show a spinner
-            document.querySelector(".checkoutBtn").disabled = true;
-            document.querySelector("#spinner").classList.remove("hidden");
-            document.querySelector("#button-text").classList.add("hidden");
+  // Calls stripe.confirmCardPayment
+  // If the card requires authentication Stripe shows a pop-up modal to
+  // prompt the user to enter authentication details without leaving your page.
+  let payWithCard = function (stripe, card, clientSecret) {
+    loading(true);
+    stripe
+      .confirmCardPayment(clientSecret, {
+        payment_method: {
+          card: card,
+        },
+      })
+      .then(function (result) {
+        if (result.error) {
+          // Show error to your customer
+          showError(result.error.message);
         } else {
-            document.querySelector(".checkoutBtn").disabled = false;
-            document.querySelector("#spinner").classList.add("hidden");
-            document.querySelector("#button-text").classList.remove("hidden");
+          // The payment succeeded!
+          orderComplete(result.paymentIntent.id);
+          fetch("/merchant/stripe/checkInfo", {
+            method: "post",
+          });
+          updateCart();
+          updateCarTotal();
         }
-    };
+      });
+  };
 
+  /* ------- UI helpers ------- */
+
+  // Shows a success message when the payment is complete
+  let orderComplete = function (paymentIntentId) {
+    loading(false);
+    document
+      .querySelector(".result-message a")
+      .setAttribute(
+        "href",
+        "https://dashboard.stripe.com/test/payments/" + paymentIntentId
+      );
+    document.querySelector(".result-message").classList.remove("hidden");
+    document.querySelector(".checkoutBtn").disabled = true;
+  };
+
+  // Show the customer the error from Stripe if their card fails to charge
+  let showError = function (errorMsgText) {
+    loading(false);
+    let errorMsg = document.querySelector("#card-error");
+    errorMsg.textContent = errorMsgText;
+    setTimeout(function () {
+      errorMsg.textContent = "";
+    }, 4000);
+  };
+
+  // Show a spinner on payment submission
+  let loading = function (isLoading) {
+    if (isLoading) {
+      // Disable the button and show a spinner
+      document.querySelector(".checkoutBtn").disabled = true;
+      document.querySelector("#spinner").classList.remove("hidden");
+      document.querySelector("#button-text").classList.add("hidden");
+    } else {
+      document.querySelector(".checkoutBtn").disabled = false;
+      document.querySelector("#spinner").classList.add("hidden");
+      document.querySelector("#button-text").classList.remove("hidden");
+    }
+  };
+}
+
+function updateCart() {
+  let removeCartItemButtons = document.getElementsByClassName("close");
+  for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i];
+    button.parentElement.parentElement.parentElement.remove();
+  }
+}
+
+function updateCarTotal() {
+  let cartItemContainers = document.getElementsByClassName("cart-items");
+  // console.log("cartItemContainer", cartItemContainer);
+  // let cartRows = cartItemContainer.getElementsByClassName("cart-row");
+  // console.log("cartRows", cartRows);
+  let total = 0;
+  let finalQuantity = 0;
+  for (let i = 0; i < cartItemContainers.length; i++) {
+    let cartItemContainer = cartItemContainers[i];
+    let priceElement =
+      cartItemContainer.getElementsByClassName("cart-price")[0];
+    let quantityElement = cartItemContainer.getElementsByClassName(
+      "cart-quantity-input"
+    )[0];
+    let price = parseFloat(priceElement.innerText.replace("$", ""));
+    quantity = quantityElement.value;
+    finalQuantity = parseInt(finalQuantity) + parseInt(quantity);
+    total = total + price * quantity;
+    // console.log("priceElement", priceElement);
+  }
+  document.getElementsByClassName("item-quantity")[0].innerHTML =
+    "Total Items: " + finalQuantity;
+  // console.log("finalQuantity", finalQuantity);
+  total = Math.round(total * 100) / 100;
+  document.getElementsByClassName("cart-total-price")[0].innerText =
+    "$ " + total;
+  document.getElementsByClassName("total-price")[0].innerText =
+    "$ " + (total + 5);
+  theEmptyCart();
 }
