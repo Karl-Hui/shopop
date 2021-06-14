67bc4be0f3dad6081f00389cd2d33f367c998999
Justin <sunnysidedigitals@gmail.com>
2021-06-15 01:46:17 +0800

    fixed dashboard

--- services/merchantService.js
+++ services/merchantService.js
@@ -60,10 +60,9 @@ class MerchantService {
         customerBuilding: customerDetail[0].building_address,
         customerCountry: customerDetail[0].country_address
       }
-      };
       // console.log("this is purchaseInfo", purchaseInfo)
       soldArr.push(purchaseInfo);
-    }
+    };
     return soldArr;
   }
 
@@ -147,38 +146,38 @@ class MerchantService {
   ) {
     return (
       knex("product_info")
-        .max("id")
-        .then((maxId) => {
-          // console.log("Max product ID", maxId)
-          let currentMax = parseInt(maxId[0].max);
-          // let currentMax = 1;
-          return currentMax;
-        })
-        .then((currentMax) => {
-          let newId = currentMax + 1;
-          let newProduct = {
-            id: newId,
-            productPhoto: JSON.stringify(productPhoto),
-            productName: productName,
-            productDescription: productDescription,
-            stock: stock,
-            price: price,
-            shippingPrice: shippingPrice,
-            Size: Size,
-            productCondition: productCondtion,
-            productCategory: productCategory,
-            productStatus: productStatus,
-            merchant_id: merchant_id,
-          };
-          // console.log("new product", newProduct)
-          return knex("product_info").insert(newProduct);
-        })
-        // .then(() => {
-        //   console.log("inserted");
-        // })
-        .catch((error) => {
-          console.log(error, "error creating new product");
-        })
+      .max("id")
+      .then((maxId) => {
+        // console.log("Max product ID", maxId)
+        let currentMax = parseInt(maxId[0].max);
+        // let currentMax = 1;
+        return currentMax;
+      })
+      .then((currentMax) => {
+        let newId = currentMax + 1;
+        let newProduct = {
+          id: newId,
+          productPhoto: JSON.stringify(productPhoto),
+          productName: productName,
+          productDescription: productDescription,
+          stock: stock,
+          price: price,
+          shippingPrice: shippingPrice,
+          Size: Size,
+          productCondition: productCondtion,
+          productCategory: productCategory,
+          productStatus: productStatus,
+          merchant_id: merchant_id,
+        };
+        // console.log("new product", newProduct)
+        return knex("product_info").insert(newProduct);
+      })
+      // .then(() => {
+      //   console.log("inserted");
+      // })
+      .catch((error) => {
+        console.log(error, "error creating new product");
+      })
     );
   }
   //************************works for one user*************************** */
@@ -238,42 +237,42 @@ class MerchantService {
   ) {
     return (
       knex("product_info")
-        .max("id")
-        .then((maxId) => {
-          // console.log("Max product ID", maxId);
-          let currentMax;
-          if (maxId[0].max) {
-            currentMax = parseInt(maxId[0].max);
-          } else {
-            currentMax = 1;
-          }
-          return currentMax;
-        })
-        .then((currentMax) => {
-          let newId = currentMax + 1;
-          let newProduct = {
-            id: newId,
-            productPhoto: JSON.stringify(productPhoto),
-            productName: productName,
-            productDescription: productDescription,
-            stock: stock,
-            price: price,
-            shippingPrice: shippingPrice,
-            Size: Size,
-            productCondition: productCondition,
-            productCategory: productCategory,
-            productStatus: productStatus,
-            merchant_id: merchant_id,
-          };
-          // console.log("new product", newProduct)
-          return knex("product_info").insert(newProduct);
-        })
-        // .then(() => {
-        //   console.log("inserted");
-        // })
-        .catch((error) => {
-          console.log(error, "error creating new product");
-        })
+      .max("id")
+      .then((maxId) => {
+        // console.log("Max product ID", maxId);
+        let currentMax;
+        if (maxId[0].max) {
+          currentMax = parseInt(maxId[0].max);
+        } else {
+          currentMax = 1;
+        }
+        return currentMax;
+      })
+      .then((currentMax) => {
+        let newId = currentMax + 1;
+        let newProduct = {
+          id: newId,
+          productPhoto: JSON.stringify(productPhoto),
+          productName: productName,
+          productDescription: productDescription,
+          stock: stock,
+          price: price,
+          shippingPrice: shippingPrice,
+          Size: Size,
+          productCondition: productCondition,
+          productCategory: productCategory,
+          productStatus: productStatus,
+          merchant_id: merchant_id,
+        };
+        // console.log("new product", newProduct)
+        return knex("product_info").insert(newProduct);
+      })
+      // .then(() => {
+      //   console.log("inserted");
+      // })
+      .catch((error) => {
+        console.log(error, "error creating new product");
+      })
     );
   }
 
@@ -281,16 +280,16 @@ class MerchantService {
     // console.log("Deleting product");
     return (
       knex("product_info")
-        .where({
-          id: id,
-        })
-        .del()
-        // .then(() => {
-        //   console.log("deleted from backend");
-        // })
-        .catch((error) => {
-          console.log("error", error);
-        })
+      .where({
+        id: id,
+      })
+      .del()
+      // .then(() => {
+      //   console.log("deleted from backend");
+      // })
+      .catch((error) => {
+        console.log("error", error);
+      })
     );
   }
 
@@ -333,16 +332,16 @@ class MerchantService {
   postMerchantImage(merchant_id, shopPictureURL) {
     return (
       this.knex("merchant_info")
-        .update("profilePic", shopPictureURL)
-        .where({
-          merchant_id: merchant_id,
-        })
-        // .then((data, dataSite) => {
-        //   console.log("added shop pic", data, dataSite);
-        // })
-        .catch((err) => {
-          console.log("err", err);
-        })
+      .update("profilePic", shopPictureURL)
+      .where({
+        merchant_id: merchant_id,
+      })
+      // .then((data, dataSite) => {
+      //   console.log("added shop pic", data, dataSite);
+      // })
+      .catch((err) => {
+        console.log("err", err);
+      })
     );
   }
 
@@ -378,27 +377,27 @@ class MerchantService {
     // console.log("updating product");
     return (
       knex("product_info")
-        .where({
-          id: id,
-        })
-        .update({
-          productPhoto: JSON.stringify(productPhoto),
-          productName: productName,
-          productDescription: productDescription,
-          stock: stock,
-          price: price,
-          shippingPrice: shippingPrice,
-          Size: Size,
-          productCondition: productCondition,
-          productCategory: productCategory,
-          productStatus: productStatus,
-        })
-        // .then(() => {
-        //   console.log("Updated product");
-        // })
-        .catch((error) => {
-          console.log(error, "Cant update product");
-        })
+      .where({
+        id: id,
+      })
+      .update({
+        productPhoto: JSON.stringify(productPhoto),
+        productName: productName,
+        productDescription: productDescription,
+        stock: stock,
+        price: price,
+        shippingPrice: shippingPrice,
+        Size: Size,
+        productCondition: productCondition,
+        productCategory: productCategory,
+        productStatus: productStatus,
+      })
+      // .then(() => {
+      //   console.log("Updated product");
+      // })
+      .catch((error) => {
+        console.log(error, "Cant update product");
+      })
     );
   }
 }
